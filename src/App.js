d7a95c8c02bd3c159d268b7ebd62cf6e38d9df78
r-ILIASS <iliass.root@gmail.com>
2021-11-04 15:58:42 +0100

    Feat: Routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Router } from './components/Router';
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <h1>App</h1>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Router />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
